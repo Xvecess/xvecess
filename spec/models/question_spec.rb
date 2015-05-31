f73f3c4dd466aa3b52ad25c2e2b5e8a276cd7350
xvecess <xvecess@mail.ru>
2015-05-31 23:08:52 +0300

    correct errors, correct association tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe Question do
+  let(:question) { create(:question) }
+  let!(:answers) { question.answers.create(body: 'Test text') }
+
   it { should validate_presence_of :title }
 
   it { should validate_presence_of :body }
 
   it { should have_many :answers }
+
+  describe 'check depend:destroy association' do
+    it 'destroy all answers for question' do
+      expect { question.destroy }.to change { question.answers.count }.by(-1)
+    end
+  end
 end
