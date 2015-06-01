b65d9113f9ce84be3053012d8da6e2cf5333565d
xvecess <xvecess@mail.ru>
2015-06-01 08:04:59 +0300

    correct tests, add validation title lenght

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   validates :title, :body, presence: true
+  validates :title, length: { in: 5..140}
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,18 +1,13 @@
 require 'rails_helper'
 
 describe Question do
-  let(:question) { create(:question) }
-  let!(:answers) { question.answers.create(body: 'Test text') }
 
   it { should validate_presence_of :title }
 
   it { should validate_presence_of :body }
 
-  it { should have_many :answers }
+  it { should have_many(:answers).dependent(:destroy) }
 
-  describe 'check depend:destroy association' do
-    it 'destroy all answers for question' do
-      expect { question.destroy }.to change { question.answers.count }.by(-1)
-    end
-  end
+  it { should validate_length_of(:title).
+            is_at_least(5).is_at_most(140) }
 end
