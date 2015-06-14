f8768843dd1cf776d8c9d3e9981c8ab697e751c8
xvecess <xvecess@mail.ru>
2015-06-14 13:09:18 +0300

    add test for displaing  best answer, correct errors

--- spec/features/user_can_select_best_answer_spec.rb
+++ spec/features/user_can_select_best_answer_spec.rb
@@ -7,14 +7,52 @@ The users were asked the question can
 
   given(:user) { create(:user) }
   given(:question) { create(:question, user_id: user.id) }
-  given!(:answer) { create(:answer, question_id: question.id,
-                           user_id: user.id, best: false) }
 
-  scenario 'user marked best answer' do
-    sign_in(user)
-    visit question_path(question)
-    click_on 'Это лучший ответ'
-    
-    expect(page).to have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+
+  context 'with one answer on page' do
+
+    given!(:answer) { create(:answer, question_id: question.id,
+                             user_id: user.id, best: false) }
+
+    scenario 'user marked best answer' do
+      sign_in(user)
+      visit question_path(question)
+
+      within "##{answer.id}" do
+        click_on 'Это лучший ответ'
+      end
+
+      expect(page).to have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+    end
+  end
+
+  context 'with two answer on page' do
+
+    given!(:answer1) { create(:answer, question_id: question.id,
+                              user_id: user.id, best: true) }
+
+    given!(:answer2) { create(:answer, question_id: question.id,
+                              user_id: user.id, best: false) }
+
+    scenario 'the best answer is displayed first' do
+      sign_in(user)
+      visit question_path(question)
+
+      within "##{answer1.id}" do
+        expect(page).to have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+      end
+      within "##{answer2.id}" do
+        expect(page).to_not have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+      end
+
+      within ("##{answer2.id}") { click_on 'Это лучший ответ' }
+
+      within "##{answer1.id}" do
+        expect(page).to_not have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+      end
+      within "##{answer2.id}" do
+        expect(page).to have_content "Пользователь #{question.user.email} отметил этот ответ, как лучший"
+      end
+    end
   end
 end
